234d28e69f00dd5e0564a32d3dbd95b121fa8bc9
Chandler Berry <chandler.r.berry@gmail.com>
2023-11-18 18:11:03 -0500

    init

---
+++ server/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+
+	"github.com/chandlerberry/grpcalc/pb"
+)
+
+type server struct {
+	pb.UnimplementedCalculatorServer
+}
+
+func (s *server) Sum(
+	ctx context.Context, in *pb.NumbersRequest,
+) (*pb.CalculationResponse, error) {
+	var sum int64
+
+	for _, num := range in.Numbers {
+		sum += num
+	}
+
+	return &pb.CalculationResponse{
+		Result: sum,
+	}, nil
+}
+
+func (s *server) Add(
+	ctx context.Context, in *pb.CalculationRequest,
+) (*pb.CalculationResponse, error) {
+	return &pb.CalculationResponse{
+		Result: in.A + in.B,
+	}, nil
+}
+
+func (s *server) Divide(
+	ctx context.Context, in *pb.CalculationRequest,
+) (*pb.CalculationResponse, error) {
+	if in.B == 0 {
+		return nil, status.Error(
+			codes.InvalidArgument, "cannot divide by zero",
+		)
+	}
+	return &pb.CalculationResponse{
+		Result: in.A / in.B,
+	}, nil
+}
+
+func main() {
+	// NOTE: gRPC only works over HTTP 2, not 1.1, hence the tcp listener
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("failed to create listener:", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+
+	pb.RegisterCalculatorServer(s, &server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalln("failed to serve:", err)
+	}
+}
